05e54286f3256f9fa7b977c241ee86ec1c81b1ff
Cattress <priyaphon.vincent@gmail.com>
2021-01-14 14:54:36 +0700

    Add project files.

---
+++ Assignment 3 Library system/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Assignment_3_Library_system
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            book harryPotter = new book("Harry Potter", "JK Rowling", 399);
+           
+            
+            
+            Console.WriteLine(harryPotter.GetTitle());
+            Console.WriteLine(harryPotter.GetAuthor());
+            Console.WriteLine(harryPotter.GetPages());
+            Console.WriteLine(harryPotter.isBorrowed());
+
+            harryPotter.BorrowBook();
+            Console.WriteLine(harryPotter.isBorrowed());
+            harryPotter.BorrowBook();
+
+            harryPotter.ReturnBook();
+            harryPotter.ReturnBook();
+
+        }
+    }
+}
---
+++ Assignment 3 Library system/book.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment_3_Library_system
+{
+    class book
+    {
+        private string title;
+        private string author;
+        private int pages;
+        private bool borrowed;
+
+        public static int NumberOfBooks = 0;
+
+        public book(string title, string author, int pages)
+        {
+            this.title = title;
+            this.author = author;
+            this.pages = pages;
+            this.borrowed = false;
+
+            NumberOfBooks = NumberOfBooks + 1;
+
+        }
+
+        public string GetTitle()
+        {
+            return title;
+        }
+
+
+        public string GetAuthor()
+        {
+            return author;
+        }
+
+        public int GetPages()
+        {
+            return pages;
+        }
+
+        public bool Ge
+        {
+        public void BorrowBook()
+        {
+            if(borrowed == false)
+            {
+                borrowed = true;
+            }
+            else
+            {
+                a
+            }
+        }
+
+        public void ReturnBook()
+        {
+            if(borrowed == true)
+            {
+                borrowed = false;
+            }
+            else
+            {
+
+            }
+
+        }
+    }    
+}
+
